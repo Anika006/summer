9eae3e77ada06a1beebba8c4fa948cd63e9dc3eb
Anika006 <anikaakther9072971@gmail.com>
2023-08-25 20:28:58 +0600

    summer section

--- js/summer.js
+++ js/summer.js
@@ -1,1 +1,25 @@
-console.log('summer');
+            
+            
+function myFunction(value) {
+    var price = document.getElementById("total_price");
+    var old_price = price.innerHTML;
+    var new_price = parseFloat(old_price) + parseFloat(value);
+    document.getElementById("total_price").innerHTML=new_price;
+    document.getElementById("total").innerHTML=new_price;
+}
+function discount(){
+    var discount = document.getElementById("discount_price").value;
+    var old = document.getElementById("total_price").innerHTML;
+    var discount_price = parseFloat(old) % parseFloat(discount);
+    document.getElementById("discount").innerHTML= discount_price;
+
+    document.getElementById("total").innerHTML = parseFloat(old) - discount_price;
+}
+function discount(){
+    var discount = document.getElementById("discount_price").value;
+    var old = document.getElementById("total_price").innerHTML;
+    var discount_price = parseFloat(old) % parseFloat(discount);
+    document.getElementById("discount").innerHTML= discount_price;
+
+    document.getElementById("total").innerHTML = parseFloat(old) - discount_price;
+}
